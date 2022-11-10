804ef98ab03289fbcfc8c18e45f6ced3b42a4951
Bimbolete <112220828+Bimbolete@users.noreply.github.com>
2022-11-10 02:39:25 -0500

    Create ICategoriaServDAO.java

---
+++ src/main/java/com/example/proyectot3/controller/model/dao/ICategoriaServDAO.java
@@ -0,0 +1,9 @@
+package com.example.proyectot3.controller.model.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.proyectot3.controller.model.entidad.CategoriaServ;
+
+public interface ICategoriaServDAO extends CrudRepository<CategoriaServ,Long>{
+    
+}
