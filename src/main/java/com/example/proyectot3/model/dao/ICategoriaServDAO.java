6fb8c51335976dd4b01cdc8ef33c041af0c052c9
Bimbolete <112220828+Bimbolete@users.noreply.github.com>
2022-11-13 14:57:20 -0500

    Update ICategoriaServDAO.java

--- src/main/java/com/example/proyectot3/model/dao/ICategoriaServDAO.java
+++ src/main/java/com/example/proyectot3/model/dao/ICategoriaServDAO.java
@@ -1,9 +1,11 @@
 package com.example.proyectot3.model.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.proyectot3.model.entidad.CategoriaServ;
 
 public interface ICategoriaServDAO extends CrudRepository<CategoriaServ,Long>{
-    
+    public List<CategoriaServ> findAllByOrderByNombre();
 }
