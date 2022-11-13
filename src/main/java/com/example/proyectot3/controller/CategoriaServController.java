0dc131d13a965b6c0e6b108b89d915e6a9f8ba1f
Bimbolete <112220828+Bimbolete@users.noreply.github.com>
2022-11-13 14:57:17 -0500

    Update CategoriaServController.java

--- src/main/java/com/example/proyectot3/controller/CategoriaServController.java
+++ src/main/java/com/example/proyectot3/controller/CategoriaServController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.proyectot3.model.entidad.CategoriaServ;
 import com.example.proyectot3.model.servicio.ICategoriaServService;
@@ -26,8 +27,9 @@ public class CategoriaServController {
     }
 
     @RequestMapping(value = "/registro",method = RequestMethod.POST)
-    public String guardar(CategoriaServ categoriaServ){
-        categoriaServService.guardarCategoriaServ(categoriaServ);
+    public String guardar(CategoriaServ categoriaServ,Model model,RedirectAttributes flash){
+        String rpta = categoriaServService.guardarCategoriaServ(categoriaServ);
+        flash.addFlashAttribute("mensaje",rpta);
         return "redirect:/categoriaServicio/";
     }
 
@@ -40,8 +42,9 @@ public class CategoriaServController {
     }
 
     @RequestMapping("eliminar/{id}")
-    public String eliminar(@PathVariable(value = "id") Long id){
-        categoriaServService.eliminarCategoriaServ(id);
+    public String eliminar(@PathVariable(value = "id") Long id,Model model,RedirectAttributes flash){
+        String rpta = categoriaServService.eliminarCategoriaServ(id);
+        flash.addFlashAttribute("mensaje",rpta);
         return "redirect:/categoriaServicio/";
     }
 }
